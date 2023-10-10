7915953ba14ba88304403d280bed5738ad6e7796
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-10 13:57:21 +0100

    Update UserDAO.kt

--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -1,41 +1,36 @@
 package ie.setu.domain.repository
 
 import ie.setu.domain.User
+import ie.setu.domain.db.Users
+import ie.setu.utils.mapToUser
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
 class UserDAO {
 
-    private val users = arrayListOf(
-        User(name = "Alice", email = "alice@wonderland.com", id = 0),
-        User(name = "Bob", email = "bob@cat.ie", id = 1),
-        User(name = "Mary", email = "mary@contrary.com", id = 2),
-        User(name = "Carol", email = "carol@singer.com", id = 3)
-    )
-
     fun getAll() : ArrayList<User> {
-        return users
+        val userList: ArrayList<User> = arrayListOf()
+        transaction {
+            Users.selectAll().map {
+                userList.add(mapToUser(it)) }
+        }
+        return userList
     }
 
     fun findById(id: Int): User?{
-        return users.find { it.id == id }
+        return null
     }
 
     fun save(user:User) {
-        users.add(user)
     }
 
     fun findByEmail(email: String): User? {
-        return users.find { it.email == email }
+        return null
     }
 
     fun delete(id: Int) {
-      val user = users.find { it.id == id }
-            users.remove(user)
     }
 
     fun update(id: Int, user: User) {
-        val editUser = users.find { it.id == id }
-        if (editUser != null) {
-            editUser.name = user.name
-            editUser.email = user.email
-        }
     }
 }
