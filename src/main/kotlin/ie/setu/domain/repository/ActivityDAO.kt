c3b1437d5e9e74061ad945c588283e69e7a3be4c
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-17 16:01:37 +0100

    Create ActivityDAO.kt

---
+++ src/main/kotlin/ie/setu/domain/repository/ActivityDAO.kt
@@ -0,0 +1,25 @@
+package ie.setu.domain.repository
+
+
+import ie.setu.domain.Activity
+import ie.setu.domain.User
+import ie.setu.domain.db.Activities
+import ie.setu.domain.db.Users
+import ie.setu.utils.mapToUser
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class ActivityDAO {
+
+    fun getAll() : ArrayList<Activity> {
+        val activityList: ArrayList<Activity> = arrayListOf()
+        transaction {
+            Activities.selectAll().map {
+                activityList.add(mapToUser(it)) }
+        }
+        return activityList
+    }
+
+
+
+}
