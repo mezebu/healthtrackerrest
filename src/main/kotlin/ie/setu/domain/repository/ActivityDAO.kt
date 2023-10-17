024c3ae4e5178ef49387f750ac9ebd9a42d3e179
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-17 16:25:52 +0100

    Update ActivityDAO.kt

--- src/main/kotlin/ie/setu/domain/repository/ActivityDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/ActivityDAO.kt
@@ -1,6 +1,5 @@
 package ie.setu.domain.repository
 
-
 import ie.setu.domain.Activity
 import ie.setu.domain.db.Activities
 import ie.setu.utils.mapToActivity
@@ -9,13 +8,46 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 class ActivityDAO {
 
-    fun getAll() : ArrayList<Activity> {
-        val activityList: ArrayList<Activity> = arrayListOf()
+    //Get all the activities in the database regardless of user id
+    fun getAll(): ArrayList<Activity> {
+        val activitiesList: ArrayList<Activity> = arrayListOf()
         transaction {
             Activities.selectAll().map {
-                activityList.add(mapToActivity(it)) }
+                activitiesList.add(mapToActivity(it)) }
         }
-        return activityList
+        return activitiesList
+    }
+
+    //Find a specific activity by activity id
+    fun findByActivityId(id: Int): Activity?{
+        return transaction {
+            Activities
+                .select() { Activities.id eq id}
+                .map{mapToActivity(it)}
+                .firstOrNull()
+        }
+    }
+
+    //Find all activities for a specific user id
+    fun findByUserId(userId: Int): List<Activity>{
+        return transaction {
+            Activities
+                .select {Activities.userId eq userId}
+                .map {mapToActivity(it)}
+        }
+    }
+
+    //Save an activity to the database
+    fun save(activity: Activity){
+        transaction {
+            Activities.insert {
+                it[description] = activity.description
+                it[duration] = activity.duration
+                it[started] = activity.started
+                it[calories] = activity.calories
+                it[userId] = activity.userId
+            }
+        }
     }
 
 }
