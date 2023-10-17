67ff883520b761813e83aef7acf81267f6f0509d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-17 16:05:27 +0100

    Update ActivityDAO.kt

--- src/main/kotlin/ie/setu/domain/repository/ActivityDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/ActivityDAO.kt
@@ -5,6 +5,7 @@ import ie.setu.domain.Activity
 import ie.setu.domain.User
 import ie.setu.domain.db.Activities
 import ie.setu.domain.db.Users
+import ie.setu.utils.mapToActivity
 import ie.setu.utils.mapToUser
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -15,7 +16,7 @@ class ActivityDAO {
         val activityList: ArrayList<Activity> = arrayListOf()
         transaction {
             Activities.selectAll().map {
-                activityList.add(mapToUser(it)) }
+                activityList.add(mapToActivity(it)) }
         }
         return activityList
     }
