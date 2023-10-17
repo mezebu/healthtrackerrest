33049ea116ecf54b9ab5723a2735c42c9a766072
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-17 13:02:51 +0100

    Update UserDAO.kt

--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -19,8 +19,7 @@ class UserDAO {
 
     fun findById(id: Int): User?{
         return transaction {
-            Users.select() {
-                Users.id eq id}
+            Users.select { Users.id eq id}
                 .map{mapToUser(it)}
                 .firstOrNull()
         }
@@ -37,7 +36,7 @@ class UserDAO {
 
     fun findByEmail(email: String): User? {
         return transaction {
-            Users.select() { Users.email eq email }
+            Users.select { Users.email eq email }
                 .map { mapToUser(it) }
                 .firstOrNull()
         }
