e222ab5ca8bc13332f01eba68cd878bfa9deb10a
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-20 14:45:19 +0100

    Update Activities.kt

--- src/main/kotlin/ie/setu/domain/db/Activities.kt
+++ src/main/kotlin/ie/setu/domain/db/Activities.kt
@@ -3,7 +3,6 @@ package ie.setu.domain.db
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
-
 // SRP - Responsibility is to manage one activity.
 //       Database wise, this is the table object.
 
