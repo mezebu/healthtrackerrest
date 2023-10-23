9cd195e2e81353edb9a69f80db19c6db512ab3c8
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-23 11:31:40 +0100

    Update HealthTrackerController.kt

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -15,43 +15,43 @@ object HealthTrackerController {
     private val userDao = UserDAO()
     private val activityDAO = ActivityDAO()
 
-    fun getAllUsers(ctx: Context) {
-        ctx.json(userDao.getAll())
-    }
-
-    fun getUserById(ctx: Context) {
-        val user = userDao.findById(ctx.pathParam("user-id").toInt())
-        if(user != null) {
-            ctx.json(user)
-        }
-    }
-
-    fun addUser(ctx: Context) {
-        val mapper = jacksonObjectMapper()
-        val user = mapper.readValue<User>(ctx.body())
-        userDao.save(user)
-        ctx.json(user)
-    }
-
-
-    fun getUsersByEmail(ctx: Context) {
-        val user = userDao.findByEmail(ctx.pathParam("email"))
-        if (user != null) {
-            ctx.json(user)
-        }
-    }
-
-    fun deleteUser(ctx: Context) {
-        userDao.delete(ctx.pathParam("user-id").toInt())
-    }
-
-    fun updateUser(ctx: Context) {
-        val userId = ctx.pathParam("user-id").toInt()
-        val mapper = jacksonObjectMapper()
-        val updatedUser = mapper.readValue<User>(ctx.body())
-        userDao.update(userId, updatedUser)
-        ctx.json(updatedUser)
-    }
+//    fun getAllUsers(ctx: Context) {
+//        ctx.json(userDao.getAll())
+//    }
+//
+//    fun getUserById(ctx: Context) {
+//        val user = userDao.findById(ctx.pathParam("user-id").toInt())
+//        if(user != null) {
+//            ctx.json(user)
+//        }
+//    }
+//
+//    fun addUser(ctx: Context) {
+//        val mapper = jacksonObjectMapper()
+//        val user = mapper.readValue<User>(ctx.body())
+//        userDao.save(user)
+//        ctx.json(user)
+//    }
+//
+//
+//    fun getUsersByEmail(ctx: Context) {
+//        val user = userDao.findByEmail(ctx.pathParam("email"))
+//        if (user != null) {
+//            ctx.json(user)
+//        }
+//    }
+//
+//    fun deleteUser(ctx: Context) {
+//        userDao.delete(ctx.pathParam("user-id").toInt())
+//    }
+//
+//    fun updateUser(ctx: Context) {
+//        val userId = ctx.pathParam("user-id").toInt()
+//        val mapper = jacksonObjectMapper()
+//        val updatedUser = mapper.readValue<User>(ctx.body())
+//        userDao.update(userId, updatedUser)
+//        ctx.json(updatedUser)
+//    }
 
     //--------------------------------------------------------------
     // ActivityDAO specifics
