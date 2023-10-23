c3badbae06e43f4a27ee3bb226cade7665cfe6e0
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-23 15:21:07 +0100

    Update Fixtures.kt

--- src/test/kotlin/ie/setu/helpers/Fixtures.kt
+++ src/test/kotlin/ie/setu/helpers/Fixtures.kt
@@ -3,11 +3,11 @@ package ie.setu.helpers
 import ie.setu.domain.User
 
 
-val nonExistingEmail = "112233445566778testUser@xxxxx.xx"
-val validName = "Test User 1"
-val validEmail = "testuser1@test.com"
+const val nonExistingEmail = "112233445566778testUser@xxxxx.xx"
+const val validName = "Test User 1"
+const val validEmail = "testuser1@test.com"
 
-val users = arrayListOf<User>(
+val users = arrayListOf(
     User(name = "Alice Wonderland", email = "alice@wonderland.com", id = 1),
     User(name = "Bob Cat", email = "bob@cat.ie", id = 2),
     User(name = "Mary Contrary", email = "mary@contrary.com", id = 3),
