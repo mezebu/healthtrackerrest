5253226cd6621171fc5e59c6e2dda7eea4153ff6
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-27 17:24:04 +0100

    Create FitnessGoals.kt

---
+++ src/main/kotlin/ie/setu/domain/db/FitnessGoals.kt
@@ -0,0 +1,17 @@
+package ie.setu.domain.db
+
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+
+// SRP - Responsibility is to manage one Fitness Goal.
+//       Database wise, this is the table object.
+
+
+object FitnessGoals: Table("fitness-goals") {
+    val id = integer("id").autoIncrement().primaryKey()
+    val goalType = varchar("type", 100)
+    val target = double("target")
+    val startDate = datetime("start_date")
+    val endDate = datetime("end_date")
+    val userId = integer("user_id").references(Users.id, onDelete = ReferenceOption.CASCADE)
+}
