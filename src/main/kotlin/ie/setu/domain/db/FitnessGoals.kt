6a633ff59f826f39a23c66ea8ec3e803831f7d69
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-28 20:44:57 +0100

    Update FitnessGoals.kt

--- src/main/kotlin/ie/setu/domain/db/FitnessGoals.kt
+++ src/main/kotlin/ie/setu/domain/db/FitnessGoals.kt
@@ -6,10 +6,11 @@ import org.jetbrains.exposed.sql.Table
 // SRP - Responsibility is to manage one Fitness Goal.
 //       Database wise, this is the table object.
 
-object FitnessGoals: Table("fitness-goals") {
+object FitnessGoals: Table("fitness_goals") {
     val id = integer("id").autoIncrement().primaryKey()
     val goalType = varchar("goal_type", 100)
     val target = double("target")
+    val calories = integer("calories")
     val startDate = datetime("start_date")
     val endDate = datetime("end_date")
     val userId = integer("user_id").references(Users.id, onDelete = ReferenceOption.CASCADE)
