de2ad5170e23205125c6337c24292ec61217b2d5
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-28 20:44:59 +0100

    Update FitnessGoal.kt

--- src/main/kotlin/ie/setu/domain/FitnessGoal.kt
+++ src/main/kotlin/ie/setu/domain/FitnessGoal.kt
@@ -6,6 +6,7 @@ data class FitnessGoal (
     var id: Int,
     var goalType:String,
     var target: Double,
+    var calories: Int,
     var startDate: DateTime,
     var endDate: DateTime,
     var userId: Int
