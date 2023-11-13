d2121eb74fe36bb6ee8a85f1759e493b1c016e34
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-13 16:39:04 +0000

    Update goal-overview.vue
    The style for the card component was removed

--- src/main/resources/vue/views/goal-overview.vue
+++ src/main/resources/vue/views/goal-overview.vue
@@ -2,7 +2,7 @@
   <app-layout>
     <div class="row">
       <div v-for="goal in goals" :key="goal.id" class="col-md-4">
-        <a :href="`/goals/${goal.id}`" style="text-decoration: none">
+        <a :href="`/goals/${goal.id}`">
           <div class="card border-info text-white mb-3">
             <div class="card-header bg-info"> Goal Id: {{ goal.id }}</div>
             <div class="card-body text-info">
