03f76ded8ea37bf7197b113cd143fbd131cf81da
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-13 14:45:42 +0000

    Refactor: Enhance structure and styling in activity card component
    Improved the structure and styling of the activity card component for better consistency.
    Unified the card header styling to use the Bootstrap 4 class structure with a primary background color.
    Ensured correct usage of the card class and fixed a typo in the second card's class attribute.
    Maintained responsiveness and readability by using the appropriate column class (col-md-4) for the card layout.

--- src/main/resources/vue/views/activity-overview.vue
+++ src/main/resources/vue/views/activity-overview.vue
@@ -3,8 +3,8 @@
       <div class="row">
         <div v-for="activity in activities" :key="activity.id" class="col-md-4">
           <a :href="`/activities/${activity.id}`">
-            <div class="card border-primary mb-3">
-              <div class="card-header"> Activity Id: {{ activity.id }}</div>
+            <div class="card border-primary text-white mb-3">
+              <div class="card-header bg-primary"> Activity Id: {{ activity.id }}</div>
               <div class="card-body text-primary">
                 <h5 class="card-title">{{activity.description}}</h5>
               </div>
