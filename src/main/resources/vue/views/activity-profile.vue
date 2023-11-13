5a888ef7b24546e9a44554933207f4de6e4082c8
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-13 14:46:44 +0000

    Refactor: Improve structure and styling in activity details component
    Enhanced the structure and styling of the activity details component for better coherence.
    Unified the card header styling to use the Bootstrap 4 class structure with a success background color and white text.
    Ensured correct usage of the card class for consistency in the card layout.
    Maintained a clean and organized presentation of activity details with appropriate list-group styling.
    This commit focuses on refining the structure and styling of the activity details component, promoting a more consistent and visually appealing design.

--- src/main/resources/vue/views/activity-profile.vue
+++ src/main/resources/vue/views/activity-profile.vue
@@ -3,7 +3,7 @@
     <div>
       <div v-if="activity">
         <div class="card border-success mb-3" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-          <div class="card-header"> Activity Id: {{ activity.id }}</div>
+          <div class="card-header bg-success text-white"> Activity Id: {{ activity.id }}</div>
           <div class="card-body">
             <h5 class="card-title">{{activity.description}}</h5>
             <ul class="list-group">
