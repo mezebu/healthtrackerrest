81f7c8045f2615857061cb366614c475d4b1dd4c
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-13 14:19:50 +0000

    Refactor: Improve UI consistency and remove redundant header in user-activity-overview component
    Explanation:
    
    Consolidated and improved the UI consistency in the user-activity-overview component by removing the redundant header "Activities list."
    Streamlined the presentation of activities by using a single card layout with consistent styling, enhancing the overall user experience.
    Ensured cleaner and more maintainable code by eliminating unnecessary duplication in the template.

--- src/main/resources/vue/views/user-activity-overview.vue
+++ src/main/resources/vue/views/user-activity-overview.vue
@@ -1,12 +1,23 @@
 <template id="user-activity-overview">
   <app-layout>
   <div>
-    <h3>Activities list </h3>
-    <ul>
-      <li v-for="activity in activities">
-        {{activity.id}}: {{activity.description}} for {{activity.duration}} minutes
-      </li>
-    </ul>
+    <h3 class="mb-3">Activities list </h3>
+    <div class="row">
+      <div v-for="activity in activities" class="col-lg-6">
+        <div class="card border-success mb-3" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+          <div class="card-header"> Activity Id: {{ activity.id }}</div>
+          <div class="card-body">
+            <h5 class="card-title">{{activity.description}}</h5>
+            <ul class="list-group">
+              <li class="list-group-item">Duration: {{activity.duration}} minutes</li>
+              <li class="list-group-item">Calories: {{activity.calories}}</li>
+              <li class="list-group-item">Started: {{activity.started}}</li>
+              <li class="list-group-item">User ID: {{activity.userId}}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   </app-layout>
 </template>
