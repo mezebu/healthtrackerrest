3cd5b1d85e43c25055ef38bc57e7d49f75a4c736
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-13 14:44:19 +0000

    Refactor: Improve structure and responsiveness in app-layout component
    Enhanced the structure of the app-layout component for better readability and maintainability.
    Updated the navigation bar to use the Bootstrap 4 class structure with proper responsiveness.
    Ensured consistent and correct usage of container elements for content organization.
    Removed unnecessary and incomplete code related to the navbar toggler for improved clarity.

--- src/main/resources/vue/components/app-layout.vue
+++ src/main/resources/vue/components/app-layout.vue
@@ -1,8 +1,9 @@
 <template id="app-layout">
   <div class="app-layout">
-    <div class="container">
+
       <!-- Start of navbar -->
-      <nav class="navbar navbar-expand-lg navbar-light">
+      <nav class="navbar navbar-expand-md navbar-light">
+        <div class="container">
         <a class="navbar-brand" href="/">Home</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse"
                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -22,16 +23,19 @@
             </li>
           </ul>
         </div>
+        </div>
       </nav>
       <!--End of nav bar-->
       <!--Start of main content area-->
+
       <div class="content mt-3">
-        <div class="container-fluid">
+        <div class="container">
           <slot></slot>
         </div>
       </div>
+
       <!--End of main content area-->
-    </div>
+
   </div>
 </template>
 
