87df5cf76e41da95c397b9cd716d30f7d29de4b3
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-13 16:35:13 +0000

    Implement goal overview component with Axios data fetching
    This commit message describes the changes made in the code. Specifically, it introduces a new Vue component named "goal-overview" that utilizes the Axios library to fetch data from the "/api/goals" endpoint. The fetched data is then displayed in a card layout for each goal, including the goal's ID and type. This commit enhances the application by introducing a dynamic goal overview component with data fetching capabilities.

---
+++ src/main/resources/vue/views/goal-overview.vue
@@ -0,0 +1,35 @@
+<template id="goal-overview">
+  <app-layout>
+    <div class="row">
+      <div v-for="goal in goals" :key="goal.id" class="col-md-4">
+        <a :href="`/goals/${goal.id}`" style="text-decoration: none">
+          <div class="card border-info text-white mb-3">
+            <div class="card-header bg-info"> Goal Id: {{ goal.id }}</div>
+            <div class="card-body text-info">
+              <h5 class="card-title">{{goal.goalType}}</h5>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+app.component("goal-overview", {
+  template: "#goal-overview",
+  data: () => ({
+    goals: [],
+  }),
+  created() {
+    this.fetchGoals();
+  },
+  methods: {
+    fetchGoals: function() {
+      axios.get("/api/goals")
+          .then(res => this.goals = res.data)
+          .catch(() => alert("Error while fitness goals"))
+    }
+  }
+})
+</script>
