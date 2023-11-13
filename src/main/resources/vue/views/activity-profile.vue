b756552fd4c5ce0639d09c21a02df21d9547a3c5
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-13 14:23:19 +0000

    feat: Implement activity-profile component
    Explanation:
    
    Introduced a new component, activity-profile, to display detailed information about a specific activity.
    Utilized the created lifecycle hook to fetch activity data based on the activity ID from the URL.
    Implemented a card layout with consistent styling for a clean and organized presentation of activity details.

---
+++ src/main/resources/vue/views/activity-profile.vue
@@ -0,0 +1,36 @@
+<template id="activity-profile">
+  <app-layout>
+    <div>
+      <div v-if="activity">
+        <div class="card border-success mb-3" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+          <div class="card-header"> Activity Id: {{ activity.id }}</div>
+          <div class="card-body">
+            <h5 class="card-title">{{activity.description}}</h5>
+            <ul class="list-group">
+              <li class="list-group-item">Duration: {{activity.duration}}</li>
+              <li class="list-group-item">Calories: {{activity.calories}}</li>
+              <li class="list-group-item">Started: {{activity.started}}</li>
+              <li class="list-group-item">User ID: {{activity.userId}}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+app.component("activity-profile", {
+  template: "#activity-profile",
+  data: () => ({
+    activity: null
+  }),
+  created: function () {
+    const activityId = this.$javalin.pathParams["activity-id"]
+    const url = `/api/activities/${activityId}`
+    axios.get(url)
+        .then(res => this.activity = res.data)
+        .catch(() => alert("Error while fetching activity" + activityId))
+  }
+})
+</script>
