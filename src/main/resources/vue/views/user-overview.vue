95eb78a7e1f9857edc98062408f5ef786fb2e213
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-11-14 13:28:21 +0000

    Implement User Overview Component with Pagination, Add/Delete Functionality, and Form Toggle
    Implemented the Vue.js user overview component, featuring pagination for a list of users, add and delete functionality with corresponding form toggle, and integration with a backend API to fetch, add, and delete users. The component includes a card layout, Bootstrap styling, and FontAwesome icons for a polished user interface. Pagination controls allow for easy navigation through the user list, ensuring a smooth user experience.

--- src/main/resources/vue/views/user-overview.vue
+++ src/main/resources/vue/views/user-overview.vue
@@ -1,22 +1,110 @@
 <template id="user-overview">
   <app-layout>
-  <div>
-    <div>
-      <ul class="user-overview-list">
-        <li v-for="user in users">
-          <a :href="`/users/${user.id}`">{{user.name}} ({{user.email}})</a>
+    <div class="card bg-light mb-3">
+      <div class="card-header">
+        <div class="row">
+          <div class="col-6">
+            Users
+          </div>
+          <div class="col" align="right">
+            <button rel="tooltip" title="Add"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="hideForm = !hideForm">
+              <i class="fa fa-plus" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card-body" :class="{ 'd-none': hideForm}">
+        <form id="addUser">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-user-name">Name</span>
+            </div>
+            <input type="text" class="form-control" v-model="formData.name" name="name" placeholder="Name"/>
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-user-email">Email</span>
+            </div>
+            <input type="email" class="form-control" v-model="formData.email" name="email" placeholder="Email"/>
+          </div>
+        </form>
+        <button rel="tooltip" title="Update" class="btn btn-info btn-simple btn-link" @click="addUser()">Add User</button>
+      </div>
+    </div>
+
+    <table class="table table-bordered table-striped table-hover">
+      <thead>
+      <tr>
+        <th scope="col" class="text-center">Name</th>
+        <th scope="col" class="text-center">Email</th>
+        <th scope="col" class="text-center">Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(user, index) in paginatedUsers" :key="index">
+        <td class="text-center">{{ user.name }}</td>
+        <td class="text-center">{{ user.email }}</td>
+        <td class="text-center">
+          <a :href="`/users/${user.id}`">
+            <button rel="tooltip" title="Update" class="btn btn-info btn-simple btn-link mx-1">
+              <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button rel="tooltip" title="Delete" class="btn btn-info btn-simple btn-link"
+                    @click="deleteUser(user, index)">
+              <i class="fas fa-trash" aria-hidden="true"></i>
+            </button>
+          </a>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+    <nav aria-label="User Pagination">
+      <ul class="pagination justify-content-center mt-3">
+        <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+          <a class="page-link" href="#" aria-label="Previous" @click="prevPage">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li class="page-item" v-for="page in totalPages" :key="page"
+            :class="{ 'active': currentPage === page }">
+          <a class="page-link" href="#" @click="setPage(page)">{{ page }}</a>
+        </li>
+        <li class="page-item" :class="{ 'disabled': currentPage === totalPages }">
+          <a class="page-link" href="#" aria-label="Next" @click="nextPage">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
         </li>
       </ul>
-    </div>
-  </div>
+    </nav>
   </app-layout>
 </template>
+
 <script>
 app.component("user-overview", {
   template: "#user-overview",
   data: () => ({
     users: [],
+    formData: { name: "", email: "" },
+    hideForm: true,
+    itemsPerPage: 5,
+    currentPage: 1,
   }),
+  computed: {
+    totalUsers: function () {
+      return this.users.length;
+    },
+    totalPages: function () {
+      return Math.ceil(this.totalUsers / this.itemsPerPage);
+    },
+    paginatedUsers: function () {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.users.slice(startIndex, endIndex);
+    },
+  },
   created() {
     this.fetchUsers();
   },
@@ -25,7 +113,52 @@ app.component("user-overview", {
       axios.get("/api/users")
           .then(res => this.users = res.data)
           .catch(() => alert("Error while fetching users"));
-    }
-  }
+    },
+
+    deleteUser: function (user, index) {
+      if (confirm('Are you sure you want to delete this user? This action cannot be undone.', 'Warning')) {
+        const userId = user.id;
+        const url = `/api/users/${userId}`;
+        axios.delete(url)
+            .then(response =>
+                this.users.splice(index, 1).push(response.data))
+            .catch(function (error) {
+              console.log(error)
+            });
+      }
+    },
+
+    addUser: function () {
+      const url = `/api/users`;
+      axios.post(url,
+          {
+            name: this.formData.name,
+            email: this.formData.email
+          })
+          .then(response => {
+            this.users.push(response.data)
+            this.hideForm= true;
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+
+    setPage: function (page) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.currentPage = page;
+      }
+    },
+    nextPage: function () {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    prevPage: function () {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+  },
 });
 </script>
