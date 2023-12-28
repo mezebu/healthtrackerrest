7c5989793c71923e90f648c389df12a5ea24adc4
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-12-28 22:09:36 +0000

    Add pagination component
    Implemented a reusable pagination component to manage navigation through pages. This component includes functionality for "Previous" and "Next" buttons, displaying individual page numbers, and updating page content through provided functions. The component ensures required props are validated and handles disabled states for navigation buttons based on the current page number.

---
+++ src/main/resources/vue/components/pagination-component.vue
@@ -0,0 +1,43 @@
+<template id="pagination-component">
+  <nav aria-label="Pagination">
+    <ul class="pagination justify-content-center mt-4">
+      <li class="page-item" :class="{ disabled: currentPage === 1 }">
+        <a class="page-link" href="#" @click.prevent="previousPage">Previous</a>
+      </li>
+      <li class="page-item" v-for="page in totalPages" :key="page" :class="{ active: currentPage === page }">
+        <a class="page-link" href="#" @click.prevent="changePage(page)">{{ page }}</a>
+      </li>
+      <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+        <a class="page-link" href="#" @click.prevent="nextPage">Next</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+app.component("pagination-component", {
+  template: "#pagination-component",
+  props: {
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+    changePage: {
+      type: Function,
+      required: true,
+    },
+    previousPage: {
+      type: Function,
+      required: true,
+    },
+    nextPage: {
+      type: Function,
+      required: true,
+    },
+  },
+});
+</script>
